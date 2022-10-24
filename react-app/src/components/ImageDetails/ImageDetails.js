b2a507c3a056846b6fcd0d8ea314f028e74780bc
matt-fong <matthewfong12@gmail.com>
2022-10-24 02:02:05 -0400

    Added CSS For Image Details Page

--- react-app/src/components/ImageDetails/ImageDetails.js
+++ react-app/src/components/ImageDetails/ImageDetails.js
@@ -2,14 +2,36 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom';
 import { deleteImageThunk } from '../../store/image';
+import { getAllImagesThunk } from '../../store/image';
+import './ImageDetails.css'
 
 const ImageDetails = () => {
 
   const dispatch = useDispatch();
+  const images = useSelector(state => state.images)
+
+  const imageId = useParams();
+
+  const currentImage = images[imageId.imageId]
+
+  useEffect(() => {
+    dispatch(getAllImagesThunk())
+  }, [])
 
   return (
-    <div onClick={() => dispatch(deleteImageThunk(4))}>
-      DELETE BUTTON
+    // <div onClick={() => dispatch(deleteImageThunk(4))}>
+    //   DELETE BUTTON
+    // </div>
+    <div className='image-detail-container'>
+
+      <div className='image-detail-top'>
+        <div>
+          <img className='image-detail-image' src={currentImage?.imageUrl}></img>
+        </div>
+      </div>
+
+      <div className='image-detail-bottom'></div>
+
     </div>
   )
 }
