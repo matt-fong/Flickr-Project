7223b8cc464924735bd24557424fda5dcb94f788
matt-fong <matthewfong12@gmail.com>
2022-10-25 01:57:22 -0400

    Comment Cards Show User First/Last Name

--- react-app/src/components/CommentCard/CommentCard.js
+++ react-app/src/components/CommentCard/CommentCard.js
@@ -10,6 +10,9 @@ const CommentCard = ({ comment }) => {
   const dispatch = useDispatch()
 
   const user = useSelector((state) => state.session.user)
+  const users = useSelector((state) => state.users)
+
+  const currentUser = users[user.id]
 
   const { imageId } = useParams()
 
@@ -23,7 +26,7 @@ const CommentCard = ({ comment }) => {
     <div className='comment-card-container'>
       <img className='comment-card-pic' src='https://icon-library.com/images/default-user-icon/default-user-icon-13.jpg'></img>
       <div className='comment-card-right-container'>
-        <div className='comment-card-name'>User's Name</div>
+        <div className='comment-card-name'>{`${currentUser?.first_name} ${currentUser?.last_name}`}</div>
         <div className='comment-card-body'>{comment?.body}</div>
       </div>
     </div>
