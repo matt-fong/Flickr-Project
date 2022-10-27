3ea2d40b7aa57cd1c87cacd2afee489016281e59
matt-fong <matthewfong12@gmail.com>
2022-10-27 01:02:41 -0400

    Added Discovr Logo On Login Form Page

--- react-app/src/components/Auth/LoginForm.js
+++ react-app/src/components/Auth/LoginForm.js
@@ -33,74 +33,89 @@ const LoginForm = () => {
   // }
 
   return (
-    <div className='login-container'>
+    <>
+      <div className='login-navbar'>
+        <div className='login-navbar-inner-container'>
 
-      <img className='login-background-image' src='https://identity.flickr.com/img/033120190455-by-Henry.26d9f56a.jpg'></img>
+          <div className='login-navbar-logo' onClick={() => history.push(`/`)} exact={true}>
+            <div className="login-navbar-dot-container">
+              <div className="login-navbar-bluedot">•</div>
+              <div className="login-navbar-pinkdot">•</div>
+            </div>
+            <div className="login-navbar-discovr">discovr</div>
+          </div>
 
-      <div className='login-form-container'>
+        </div>
+      </div>
+      <div className='login-container'>
 
-        <div className='login-form-inner-container'>
+        <img className='login-background-image' src='https://identity.flickr.com/img/033120190455-by-Henry.26d9f56a.jpg'></img>
 
-          <form className='login-form' onSubmit={onLogin}>
+        <div className='login-form-container'>
 
-            <div className="login-form-dot-icon">
-              <div className="login-form-dot-container">
-                <div className="login-form-bluedot">•</div>
-                <div className="login-form-pinkdot">•</div>
+          <div className='login-form-inner-container'>
+
+            <form className='login-form' onSubmit={onLogin}>
+
+              <div className="login-form-dot-icon">
+                <div className="login-form-dot-container">
+                  <div className="login-form-bluedot">•</div>
+                  <div className="login-form-pinkdot">•</div>
+                </div>
               </div>
-            </div>
 
-            <div className='login-form-login-container'>
-              <div className='login-form-login'>Log in to Discovr</div>
+              <div className='login-form-login-container'>
+                <div className='login-form-login'>Log in to Discovr</div>
 
-            </div>
+              </div>
 
-            {/* <div>
-              {errors.map((error, ind) => (
-                <div key={ind}>{error}</div>
-              ))}
-            </div> */}
+              {/* <div>
+                {errors.map((error, ind) => (
+                  <div key={ind}>{error}</div>
+                ))}
+              </div> */}
 
-            <div className='login-form-input-container'>
-              {/* <label htmlFor='email'>Email</label> */}
-              <input
-                className='login-input'
-                type='text'
-                placeholder='Email'
-                value={email}
-                onChange={updateEmail}
-                required
-              />
-            </div>
+              <div className='login-form-input-container'>
+                {/* <label htmlFor='email'>Email</label> */}
+                <input
+                  className='login-input'
+                  type='text'
+                  placeholder='Email'
+                  value={email}
+                  onChange={updateEmail}
+                  required
+                />
+              </div>
 
-            <div className='login-form-input-container'>
-              {/* <label htmlFor='password'>Password</label> */}
-              <input
-                className='login-input'
-                type='password'
-                placeholder='Password'
-                value={password}
-                onChange={updatePassword}
-                required
-              />
-            </div>
+              <div className='login-form-input-container'>
+                {/* <label htmlFor='password'>Password</label> */}
+                <input
+                  className='login-input'
+                  type='password'
+                  placeholder='Password'
+                  value={password}
+                  onChange={updatePassword}
+                  required
+                />
+              </div>
 
-            <div className='login-form-button-container'>
-              <button className='login-button' type='submit'>Sign in</button>
-            </div>
+              <div className='login-form-button-container'>
+                <button className='login-button' type='submit'>Sign in</button>
+              </div>
 
-            <div className='login-signup-container'>
-              <div className='login-signup-one'>Not a Discovr member?</div>
-              <div className='login-signup-two' onClick={() => history.push(`/sign-up`)}>Sign up here.</div>
-            </div>
+              <div className='login-signup-container'>
+                <div className='login-signup-one'>Not a Discovr member?</div>
+                <div className='login-signup-two' onClick={() => history.push(`/sign-up`)}>Sign up here.</div>
+              </div>
 
-          </form>
+            </form>
+
+          </div>
 
         </div>
 
       </div>
-
-    </div>
+    </>
   );
 };
 
