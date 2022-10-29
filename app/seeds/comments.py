0e51a3a3716809f8312ad19f1ec3ecccb8383ab5
matt-fong <matthewfong12@gmail.com>
2022-10-28 20:35:08 -0400

    Fixing Seeders For Heroku

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -3,11 +3,11 @@ from app.models import db, Comment
 
 def seed_comments():
   one = Comment(
-    body='Body1', userId=1, imageId=1)
+    body='Awesome photo.', userId=1, imageId=1)
   two = Comment(
-    body='Body2', userId=2, imageId=2)
+    body='Nice!', userId=2, imageId=2)
   three = Comment(
-    body='Body3', userId=3, imageId=3)
+    body='Amazing!', userId=3, imageId=3)
 
   db.session.add(one)
   db.session.add(two)
