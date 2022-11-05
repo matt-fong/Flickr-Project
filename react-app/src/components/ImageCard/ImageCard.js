dac49a8ac8af07aa5f2816968ec5c51f16862b61
matt-fong <matthewfong12@gmail.com>
2022-11-05 02:27:15 -0400

    Handling Edge Cases For Create Image/Image Card Overflow/CSS Sizing

--- react-app/src/components/ImageCard/ImageCard.js
+++ react-app/src/components/ImageCard/ImageCard.js
@@ -13,6 +13,34 @@ const ImageCard = ({ image }) => {
 
   const imageOwner = users[image?.userId]
 
+  let title;
+  let firstname;
+  let lastname;
+
+  if (image?.title?.length > 18) {
+    title = (image?.title)?.slice(0, 18) + '...'
+  } else {
+    title = (image?.title)
+  }
+
+  if (imageOwner?.first_name?.length > 15) {
+    firstname = (imageOwner?.first_name)?.slice(0, 15)+ '...'
+  } else {
+    firstname = (imageOwner?.first_name)?.slice(0, 15)
+  }
+
+  if (imageOwner?.last_name?.length > 15) {
+    lastname = (imageOwner?.last_name)?.slice(0, 15) + '...'
+  } else {
+    lastname = (imageOwner?.last_name)?.slice(0, 15)
+  }
+
+  if (imageOwner?.last_name?.length + imageOwner?.first_name?.length > 30) {
+    lastname = ''
+  } else {
+    lastname = (imageOwner?.last_name)
+  }
+
   useEffect(() => {
     dispatch(getAllImagesThunk())
     dispatch(getAllUsersThunk())
@@ -21,13 +49,13 @@ const ImageCard = ({ image }) => {
 
   return (
     <div className="image-card-container" key={''}>
-      <Link to={`/image/${image.id}`}>
-        <img className="image-card-image" src={image.imageUrl} alt=""
+      <Link to={`/image/${image?.id}`}>
+        <img className="image-card-image" src={image?.imageUrl} alt=""
         onError={e => { e.currentTarget.src = "https://demofree.sirv.com/nope-not-here.jpg"; }}
         ></img>
         <div className="image-card-text-container">
-          <div className="image-card-text-title">{image.title}</div>
-          <div className="image-card-text-name">{`by ${imageOwner?.first_name} ${imageOwner?.last_name}`}</div>
+          <div className="image-card-text-title">{title}</div>
+          <div className="image-card-text-name">{`by ${firstname} ${lastname}`}</div>
         </div>
       </Link>
     </div>
