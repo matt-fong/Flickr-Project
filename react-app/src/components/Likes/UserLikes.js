241d20761afb6ad625445e3cd80826e3af618ed6
matt-fong <matthewfong12@gmail.com>
2022-11-11 18:41:47 -0500

    Added CSS To UserLikes Page

--- react-app/src/components/Likes/UserLikes.js
+++ react-app/src/components/Likes/UserLikes.js
@@ -4,6 +4,7 @@ import { getAllLikesThunk } from '../../store/like';
 import { getAllImagesThunk } from '../../store/image';
 import ImageCard from "../ImageCard/ImageCard";
 import './UserLikes.css'
+import { getAllUsersThunk } from '../../store/user';
 
 const UserLikes = () => {
 
@@ -14,6 +15,9 @@ const UserLikes = () => {
   const images = useSelector((state) => state.images)
 
   const likesArr = Object.values(likes)
+
+  const currentUser = useSelector((state) => state.users[user?.id])
+  console.log('currentUser', currentUser)
 
   const userLikes = likesArr.filter(like => like?.userId === user?.id)
 
@@ -40,22 +44,40 @@ const UserLikes = () => {
   useEffect(() => {
     dispatch(getAllImagesThunk())
     dispatch(getAllLikesThunk())
+    dispatch(getAllUsersThunk())
   }, [dispatch])
 
   return (
-    <div className="explorepage-container">
-      <div className="explorepage-inner-container">
-        <div className="explorepage-images-container">
-          {userLikedImages.map((image) => {
-            return (
-              <>
-                <ImageCard image={image}/>
-              </>
-            );
-          })}
+    <>
+      <div className='userlikes-top-container'>
+        <img className='userlikes-top' src={'https://combo.staticflickr.com/pw/images/coverphoto11_h.jpg.v3'} />
+        <div className='userlikes-inner-container'>
+
+          <div className='userlikes-test'>
+            <img className='userlikes-profile' src='https://www.seekpng.com/png/full/73-730482_existing-user-default-avatar.png' alt=''></img>
+            <div className='userlikes-info'>
+              <div className='userlikes-name'>{`${currentUser?.first_name} ${currentUser?.last_name}`}</div>
+              <div className='userlikes-username'>{currentUser?.username}</div>
+            </div>
+          </div>
+
         </div>
       </div>
-    </div>
+
+      <div className="userlikes-container">
+        <div className="userlikes-inner-container">
+          <div className="userlikes-images-container">
+            {userLikedImages.map((image) => {
+              return (
+                <>
+                  <ImageCard image={image}/>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
   )
 
 }
