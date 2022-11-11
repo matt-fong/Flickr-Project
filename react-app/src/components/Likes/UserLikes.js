350ce311f673a5ec2ba45667fce42adf15d64917
matt-fong <matthewfong12@gmail.com>
2022-11-11 15:34:37 -0500

    Changed Naming Convention On Likes/Code Clean Up

--- react-app/src/components/Likes/UserLikes.js
+++ react-app/src/components/Likes/UserLikes.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { createLikeThunk, getAllLikesThunk } from '../../store/like';
 import { getAllImagesThunk } from '../../store/image';
 import { deleteLikeThunk } from '../../store/like';
@@ -8,37 +8,27 @@ import './UserLikes.css'
 
 const UserLikes = ({ imageId }) => {
 
+  const dispatch = useDispatch();
+
   const user = useSelector((state) => state.session.user)
   const likes = useSelector((state) => state.likes)
   const images = useSelector((state) => state.images)
 
-  // console.log("THIS IS THE IMAGE ID", imageId)
+  const likesArr = Object.values(likes)
 
-  const likesarr = Object.values(likes)
-  console.log('THIS IS LIKES', likesarr)
-
-  let testarr = [];
-
-  for (let i = 0; i < likesarr.length; i++) {
-    testarr.push(likesarr[i].imageId)
+  let likesImageIdArr = [];
+  for (let i = 0; i < likesArr.length; i++) {
+    likesImageIdArr.push(likesArr[i].imageId)
   }
-
-  // console.log('THIS IS TESTARR', testarr)
 
   const imagearr = Object.values(images)
 
-  const newimagearr = [];
-
+  const userLikedImages = [];
   for (let i = 0; i < imagearr.length; i++) {
-    if (testarr.includes(imagearr[i].id)) {
-      newimagearr.push(imagearr[i])
+    if (likesImageIdArr.includes(imagearr[i].id)) {
+      userLikedImages.push(imagearr[i])
     }
   }
-
-  // console.log('THIS IS FINALNEWIMAGEARR', newimagearr)
-
-  const history = useHistory()
-  const dispatch = useDispatch();
 
   const handleCreateLike = (e) => {
     e.preventDefault();
@@ -54,9 +44,9 @@ const UserLikes = ({ imageId }) => {
   const handleDeleteLike = (e) => {
     e.preventDefault();
 
-    for (let i = 0; i < likesarr.length; i++) {
-      if (likesarr[i]?.userId === user?.id && likesarr[i]?.imageId === Number(imageId)) {
-        return dispatch(deleteLikeThunk(likesarr[i]?.id))
+    for (let i = 0; i < likesArr.length; i++) {
+      if (likesArr[i]?.userId === user?.id && likesArr[i]?.imageId === Number(imageId)) {
+        return dispatch(deleteLikeThunk(likesArr[i]?.id))
       }
     }
 
@@ -64,31 +54,17 @@ const UserLikes = ({ imageId }) => {
 
   let conditional;
 
-  for (let i = 0; i < likesarr.length; i++) {
-    // console.log('THIS IS ITERATION', likesarr[i])
-    // console.log('THIS IS LIKES USERID', likesarr[i]?.userId)
-    // console.log('THIS IS USER ID', user?.id)
-    // console.log('THIS IS LIKES IMAGE ID', likesarr[i]?.imageId)
-    // console.log('THIS IS IMAGE ID', imageId)
-    if ((likesarr[i]?.userId === user?.id) && (likesarr[i]?.imageId === Number(imageId))) {
-      console.log('THIS IS TRUE')
-    } else {
-      console.log('THIS IS FALSE')
-    }
-  }
-
-
-  for (let i = 0; i < likesarr.length; i++) {
-    if (likesarr[i]?.userId === user?.id && likesarr[i]?.imageId === Number(imageId)) {
+  for (let i = 0; i < likesArr.length; i++) {
+    if (likesArr[i]?.userId === user?.id && likesArr[i]?.imageId === Number(imageId)) {
       conditional = (
         <div onClick={handleDeleteLike}>
-          <i class="fa-solid fa-heart">DELETE</i>
+          <i class="fa-solid fa-heart fa-2xl delete-like-button"></i>
         </div>
       )
     } else {
       conditional = (
         <div onClick={handleCreateLike}>
-          <i class="fa-regular fa-heart fa-2xl yessir"></i>
+          <i class="fa-regular fa-heart fa-2xl create-like-button"></i>
         </div>
       )
     }
