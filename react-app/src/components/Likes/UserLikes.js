afcc9c3b8ab89c3977bd3638341742b4d96640c3
matt-fong <matthewfong12@gmail.com>
2022-11-11 02:08:37 -0500

    Filtering Images Array For Likes By User

--- react-app/src/components/Likes/UserLikes.js
+++ react-app/src/components/Likes/UserLikes.js
@@ -1,20 +1,61 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { useHistory, useParams } from 'react-router-dom';
-import { getAllLikesThunk } from '../../store/like';
+import { createLikeThunk, getAllLikesThunk } from '../../store/like';
+import { getAllImagesThunk } from '../../store/image';
 
 const UserLikes = () => {
+
+  const user = useSelector((state) => state.session.user)
+  const likes = useSelector((state) => state.likes)
+  const images = useSelector((state) => state.images)
+
+  const likesarr = Object.values(likes)
+  console.log('THIS IS LIKES', likesarr)
+
+  let testarr = [];
+
+  for (let i = 0; i < likesarr.length; i++) {
+    testarr.push(likesarr[i].imageId)
+  }
+
+  console.log('THIS IS TESTARR', testarr)
+
+  const imagearr = Object.values(images)
+
+  const newimagearr = [];
+
+  for (let i = 0; i < imagearr.length; i++) {
+    if (testarr.includes(imagearr[i].id)) {
+      newimagearr.push(imagearr[i])
+    }
+  }
+
+  console.log('THIS IS NEWIMAGEARR', newimagearr)
 
   const history = useHistory()
   const dispatch = useDispatch();
 
+  const handleCreateLike = (e) => {
+    e.preventDefault();
+
+    const data = {
+      userId: user.id,
+      imageId: 15,
+    }
+
+    return dispatch(createLikeThunk(data))
+
+  }
+
   useEffect(() => {
+    dispatch(getAllImagesThunk())
     dispatch(getAllLikesThunk())
   }, [dispatch])
 
   return (
-    <div>
-      TESTING
+    <div onClick={handleCreateLike}>
+      CREATE LIKE
     </div>
   )
 
