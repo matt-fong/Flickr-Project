caea6a2b6dae488decd8db94bd22a47782e7055f
matt-fong <matthewfong12@gmail.com>
2022-11-12 18:08:50 -0500

    Create/Delete Tag Routes Working On Front End But No CSS

--- react-app/src/components/Tags/TagCard.js
+++ react-app/src/components/Tags/TagCard.js
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { useHistory, useParams } from 'react-router-dom';
 import { getAllTagsThunk } from '../../store/tag';
+import { createTagThunk } from '../../store/tag';
+import { deleteTagThunk } from '../../store/tag';
 
 const TagCard = () => {
 
@@ -9,8 +11,28 @@ const TagCard = () => {
   const dispatch = useDispatch();
 
   const tags = useSelector((state) => state.tags)
+  const user = useSelector((state) => state.session.user)
 
   console.log('THIS IS TAGS', tags)
+
+  const handleCreateTag = (e) => {
+    e.preventDefault();
+
+    const data = {
+      userId: user.id,
+      imageId: 1,
+      name: 'TESTING NEW'
+    }
+
+    return dispatch(createTagThunk(data))
+  }
+
+  const handleDeleteTag = (e) => {
+    e.preventDefault();
+
+    return dispatch(deleteTagThunk(1))
+
+  }
 
   useEffect(() => {
     dispatch(getAllTagsThunk())
@@ -18,7 +40,10 @@ const TagCard = () => {
 
 
   return (
-    <div>TESTING</div>
+    <div>
+      <div onClick={handleCreateTag}>Create Tag</div>
+      <div onClick={handleDeleteTag}>Delete Tag</div>
+    </div>
   )
 }
 
