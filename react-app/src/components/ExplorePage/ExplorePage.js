963f100e0fcca1f8edc06b2d189dd22f2a692c15
matt-fong <matthewfong12@gmail.com>
2022-11-14 01:40:01 -0500

    Adding Explore/Tags Tab In Explore Page

--- react-app/src/components/ExplorePage/ExplorePage.js
+++ react-app/src/components/ExplorePage/ExplorePage.js
@@ -4,9 +4,11 @@ import { getAllImagesThunk } from "../../store/image";
 import './ExplorePage.css';
 import ImageCard from "../ImageCard/ImageCard";
 import { getAllCommentsThunk } from "../../store/comment";
+import { useHistory, useParams } from 'react-router-dom';
 
 const ExplorePage = () => {
 
+  const history = useHistory();
   const dispatch = useDispatch();
 
   const images = useSelector(state => state.images)
@@ -20,19 +22,29 @@ const ExplorePage = () => {
 
 
   return (
-    <div className="explorepage-container">
-      <div className="explorepage-inner-container">
-        <div className="explorepage-images-container">
-          {imagesArr.map((image) => {
-            return (
-              <>
-                <ImageCard image={image} comments={comments}/>
-              </>
-            );
-          })}
+    <>
+
+      <div className="explorepage-tabs-container">
+        <div className="explorepage-tabs">
+          <div className="explorepage-tab-explore" onClick={() => history.push(`/explore`)}>Explore</div>
+          <div className="explorepage-tab-tags" onClick={() => history.push(`/tags`)}>Tags</div>
         </div>
       </div>
-    </div>
+
+      <div className="explorepage-container">
+        <div className="explorepage-inner-container">
+          <div className="explorepage-images-container">
+            {imagesArr.map((image) => {
+              return (
+                <>
+                  <ImageCard image={image} comments={comments}/>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
