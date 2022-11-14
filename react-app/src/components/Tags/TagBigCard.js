fc67b78c2f81f351beb5c47e9a8cd496e4cd73e9
matt-fong <matthewfong12@gmail.com>
2022-11-14 02:38:08 -0500

    Tag Cards Are Created With Random Images

--- react-app/src/components/Tags/TagBigCard.js
+++ react-app/src/components/Tags/TagBigCard.js
@@ -5,9 +5,19 @@ const TagBigCard = ({ tag }) => {
 
   const history = useHistory();
 
+  const images = [
+    'https://www.pixel4k.com/wp-content/uploads/2018/09/night-city-city-lights-metropolis-night-4k_1538067561.jpg',
+    'https://uploads-ssl.webflow.com/6212e72bb840050c246ea51b/622dbb87b588c8f2069161cb_derive-japan-cody-ellingham.jpeg',
+    'https://photographylife.com/wp-content/uploads/2016/06/Mass.jpg'
+  ]
+
+  const randomNum = Math.floor(Math.random() * images.length)
+
+  const randomImage = images[randomNum]
+
   return (
     <div className='tagbigcard-container' onClick={() => history.push(`/tag/${tag?.name}`)}>
-      <img className='tagbigcard-pic' src='https://www.pixel4k.com/wp-content/uploads/2018/09/night-city-city-lights-metropolis-night-4k_1538067561.jpg'></img>
+      <img className='tagbigcard-pic' src={randomImage}></img>
       <div className='tagbigcard-text'>{tag?.name}</div>
     </div>
   )
