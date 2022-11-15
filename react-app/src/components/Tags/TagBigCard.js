56dc52e3dbf251060315c7cdda4e243072870101
matt-fong <matthewfong12@gmail.com>
2022-11-14 19:24:08 -0500

    Updated Images For TagBigCard

--- react-app/src/components/Tags/TagBigCard.js
+++ react-app/src/components/Tags/TagBigCard.js
@@ -1,25 +1,58 @@
-import { useHistory } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react';
+import { getAllTagsThunk } from '../../store/tag';
+import { getAllImagesThunk } from '../../store/image';
+import ImageCard from "../ImageCard/ImageCard";
+import { getAllCommentsThunk } from "../../store/comment";
+import { getAllLikesThunk } from '../../store/like';
+import { getAllUsersThunk } from '../../store/user';
 import './TagBigCard.css'
 
 const TagBigCard = ({ tagname }) => {
 
+  const dispatch = useDispatch();
   const history = useHistory();
 
-  const images = [
-    'https://www.pixel4k.com/wp-content/uploads/2018/09/night-city-city-lights-metropolis-night-4k_1538067561.jpg',
-    'https://uploads-ssl.webflow.com/6212e72bb840050c246ea51b/622dbb87b588c8f2069161cb_derive-japan-cody-ellingham.jpeg',
-    'https://photographylife.com/wp-content/uploads/2016/06/Mass.jpg',
-    'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg',
-    'https://shotkit.com/wp-content/uploads/2020/08/night-landscape-photography-featured.jpg',
-  ]
+  const tags = useSelector(state => state.tags);
+  const tagsArr = Object.values(tags);
 
-  const randomNum = Math.floor(Math.random() * images.length)
+  const images = useSelector(state => state.images);
+  const imagesArr = Object.values(images);
 
-  const randomImage = images[randomNum]
+  // Filters all the tags that matches the tagname in url
+  const filteredTags = tagsArr.filter(tag => tag.name === tagname);
+
+  // Loops through the filtered tags and pushes all the imageIds of those tags into an array
+  let eventIdArr = [];
+
+  for (let i = 0; i < filteredTags.length; i++) {
+    if (!eventIdArr.includes(filteredTags[i].imageId)) {
+      eventIdArr.push(filteredTags[i].imageId)
+    }
+  }
+
+  // Filters all the images that matches the imageIds in the eventIdArr (All the images that have the tagname in url)
+  const filteredByTag = imagesArr.filter(image => eventIdArr.includes(image.id))
+
+  // Filters all the images whose title matches the tagname in url
+  const filteredByTitle = imagesArr.filter(image => image.title.toLowerCase().includes(tagname.toLowerCase()))
+
+  // Comining the two arrays of images (Tagged and Title) into a single array
+  const finalFiltered = filteredByTag.concat(filteredByTitle)
+
+  const randomNum = Math.floor(Math.random() * finalFiltered.length)
+
+  const randomImage = finalFiltered[randomNum]
+
+  useEffect(() => {
+    dispatch(getAllTagsThunk())
+    dispatch(getAllImagesThunk())
+  }, [dispatch])
 
   return (
     <div className='tagbigcard-container' onClick={() => history.push(`/tags/${tagname}`)}>
-      <img className='tagbigcard-pic' src={randomImage} alt=''></img>
+      <img className='tagbigcard-pic' src={randomImage?.imageUrl} alt=''></img>
       <div className='tagbigcard-text'>{tagname}</div>
     </div>
   )
